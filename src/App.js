7337ff21d425cde3099868aed498a86983f677bd
jdrg7 <jgarciadani0302@gmail.com>
2022-07-11 21:34:47 -0700

    avances con react

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
+import { useState, useEffect } from 'react';
 import Splash from './Views/Splash';
+import Routes from './Routes';
 
 function App() {
+  const [appLoaded, setAppLoaded] = useState(false);
+  useEffect(() => {
+    setTimeout(() => {
+      setAppLoaded(true);
+    }, 3000);
+  }
+    , []);
   return (
-    <Splash>
-
-    </Splash>
+    <>
+      {!appLoaded && <Splash />}
+      {appLoaded && <Routes />}
+    </>
   );
 }
 
--- src/Components/Page/index.js
+++ src/Components/Page/index.js
@@ -1,21 +1,22 @@
-import './page.css';
 import NavBar from '../NavBar';
-const Page = ({ children, useAbsoluteCenter=false, showNavBar = true, pageTitle = "..." }) => {
-    let className = ['page'];
-    if (useAbsoluteCenter) {
-        className.push('absoluteCenter');
-    }
-    if (showNavBar){
-        className.push('withNavBar');
-    }
-    return (
-        <>
-            {showNavBar && <NavBar title={ pageTitle} />}
-            <section className={className.join(' ')}>
-                {children}
-            </section>
-        </>
-    )
+import './page.css';
+const Page = ({ children, useAbsoluteCenter = false, showNavBar = true, pageTitle = "..." }) => {
+  let className = ['page'];
+  if (useAbsoluteCenter) {
+    className.push('absoluteCenter');
+  }
+  if (showNavBar) {
+    className.push('withNavBar');
+  }
+
+  return (
+    <>
+      {showNavBar && <NavBar title={pageTitle} />}
+      <section className={className.join(' ')}>
+        {children}
+      </section>
+    </>
+  );
 }
 
 export default Page;
